38bac2ccc29b451e8565d97c4f11b80983a3419a
Prasan <42831073+prasan321@users.noreply.github.com>
2021-06-10 11:05:27 +0800

    2nd

--- oop_basics.py
+++ oop_basics.py
@@ -1,2 +1,37 @@
-oop_basics.py
-print('hello oop')
+'''
+Python OOP Concepts - Basics
+'''
+
+#basic class
+class Animal:
+    pass
+
+print(type(Animal))
+
+class Person:
+    #methods
+    def set_details(self,name,age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print('I am', self.name)
+    
+    def greet(self):
+        if self.age < 80:
+            print('Hello, how are you doing?')
+        else:
+            print('Hello, how do you do?')
+        self.display()
+
+#create person object
+p1 = Person()
+p2 = Person()
+
+#call set_details method to set the person details
+p1.set_details('Prasan', 33)
+p2.set_details('Mike', 30)
+
+#call greet function
+p1.greet()
+p2.greet()
